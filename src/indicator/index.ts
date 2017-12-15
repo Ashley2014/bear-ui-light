1936578f81fd2db934ff150f4466e0f8f274c55b
Ashley <1746534101@qq.com>
2017-12-15 10:41:20 +0800

    1

--- src/indicator/index.ts
+++ src/indicator/index.ts
@@ -1,1 +1,3 @@
 console.log(23424)
+
+
---
+++ webpack.prod.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const info=require('./package.json')
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  // devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader:require.resolve('ts-loader'),
+          options: {
+            transpileOnly: true,
+            "compilerOptions": {
+              "module": "commonjs",
+              "target": "es5",
+              "sourceMap": false
+            },
+            // "exclude": [
+            //   "node_modules"
+            // ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+  ],
+  output: {
+    filename: `bear-ui-lite.min.js`,
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/",
+  }
+};
