39a87ed1f969cac4f269432d15ea00cbecd79c97
Ashley <1746534101@qq.com>
2017-12-15 16:09:06 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,60 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const webpack = require('webpack');
 
+let styleLoaderUseConfig1={
+  loader: require.resolve('style-loader'),
+  options: {
+    sourceMap: true,
+  },
+}
+
+let styleLoaderUseConfig2_1={
+  loader: require.resolve('css-loader'),
+  options: {
+    importLoaders: 1,
+    sourceMap: true,
+    modules:true,
+    localIdentName:"[name]__[local]___[hash:base64:5]",
+  },
+}
+let styleLoaderUseConfig2_2={
+  loader: require.resolve('css-loader'),
+  options: {
+    importLoaders: 1,
+    sourceMap: true,
+  },
+}
+
+let styleLoaderUseConfig3={
+  loader: require.resolve('postcss-loader'),
+  options: {
+    // Necessary for external CSS imports to work
+    // https://github.com/facebookincubator/create-react-app/issues/2677
+    sourceMap: true,
+    ident: 'postcss',
+    plugins: () => [
+      require('postcss-flexbugs-fixes'),
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9', // React doesn't support IE8 anyway
+        ],
+        flexbox: 'no-2009',
+      }),
+    ],
+  },
+}
+
+
+let styleLoadersUseConfigs1=[
+  styleLoaderUseConfig1,
+  styleLoaderUseConfig2_1,
+  styleLoaderUseConfig3,
+]
+
+
 module.exports = {
   entry: {
     app: './src/index.js',
@@ -34,7 +88,28 @@ module.exports = {
             // ]
           }
         }
-      }
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            // attrs: [':data-src']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            // loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
     ]
   },
   plugins: [
